755a8945eaaa0cc8e46e8b523b2877c5fde90125
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-27 21:21:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,15 +9,16 @@ class School
     @name= name
   end
 
+  def add_student(name, grade)
+    ROSTER[grade] = []
+    ROSTER[grade] << name
+  end
+
   def roster
       @roster = roster
       ROSTER
   end
 
-  def add_student(name, grade)
-    ROSTER[grade] = []
-    ROSTER[grade] << name
-  end
 
   def grade(grade)
     ROSTER[grade]
